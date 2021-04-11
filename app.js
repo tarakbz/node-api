7ee82ab213978bac064a2f99e0e85bc5029f8445
tarak <bouziri.tarak@gmail.com>
2021-04-11 01:50:54 -0400

    middleware application

--- app.js
+++ app.js
@@ -3,24 +3,39 @@ const {success} = require('./helper');
 const pokemons = require('./mock-pokemon');
 const app = express();
 
-const port = 3000;
+// exemple plus simple
+// const logger = (req, res, next) => {
+//     console.log("Time: " + Date.now());
+//     console.log("Url: " + req.url);
+//     next();
+// }
+// // middleware application
+// app.use(logger);
+
+// middleware application
+app.use((req, res, next) => {
+    console.log("Time: " + Date.now());
+    console.log("Url: " + req.url);
+    next();
+});
+
 
 app.get("/", (req, res) => res.send("hello express 3"));
 
-
-app.get("/pokemon/:id", (req, res) => {
+app.get("/pokemons/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = "un pokemon a ete trouver";
-
     res.json(success(message, pokemon));
 });
 
 
 app.get("/pokemons", (req, res) => {
-    res.send("vous avez " + pokemons.length + " pokemons")
+    const message = "la liste des pokemons a bien etait retourner";
+    res.json(success(message, pokemons));
 });
 
+const port = 3000;
 app.listen(port, () => {
     console.log("notre application ecoute http://localhost:" + port + "/")
 })
