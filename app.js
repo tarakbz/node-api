de16ea067c9f3835eb33cd6fecb96f88879f5e73
tarak <bouziri.tarak@gmail.com>
2021-04-26 03:45:28 -0400

    Refacto de app.js

--- app.js
+++ app.js
@@ -2,100 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-const {Sequelize, DataTypes} = require("sequelize");
-
-const {success, getUniqueId} = require('./helper');
-let pokemons = require('./mock-pokemon');
-
-// models
-const PokemonModel = require('./src/models/pokemon');
-
-const sequelize = new Sequelize(
-    'pokedex',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-);
-
-sequelize.authenticate()
-    .then(_ => console.log("connection a la base de donnees a bien ete effectuer"))
-    .catch(error => console.log('Impossible de se connecter a la base de donnees ' + error));
-
-// creation de la table
-const Pokemon = PokemonModel(sequelize, DataTypes);
-
-sequelize.sync({force: true})
-    .then(
-        _ => {
-            console.log("la base de donnees a bien etait synchronisee.");
-            pokemons.map(pokemon => {
-                // ajout d'un pokemon dans la table
-                Pokemon.create({
-                    name: pokemon.name,
-                    hp: pokemon.hp,
-                    cp: pokemon.cp,
-                    picture: pokemon.picture,
-                    types: pokemon.types.join(),
-                    created: new Date()
-                }).then(pokemon => console.log(pokemon.toJSON()))
-            });
-
-        }
-    );
+const sequelize = require('./src/db/sequelize');
 
 const app = express();
+const port = 3000;
 
-
-app
-    .use(favicon(__dirname + '/favicon.ico'))
+app.use(favicon(__dirname + '/favicon.ico'))
     .use(morgan('dev'))
     .use(bodyParser.json());
 
+sequelize.initDb();
 
-app.get("/", (req, res) => res.send("hello express 3"));
-
-app.get("/pokemons/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    const message = "un pokemon a ete trouver";
-    res.json(success(message, pokemon));
-});
-
-
-app.get("/pokemons", (req, res) => {
-    const message = "la liste des pokemons a bien etait retourner";
-    res.json(success(message, pokemons));
-});
-
-app.post('/pokemons', (req, res) => {
-    const pokemonCreated = {...req.body, ...{id: getUniqueId(pokemons), created: new Date()}};
-    pokemons.push(pokemonCreated);
-    const message = "un pokemon a ete ajouter";
-    res.json(success(message, pokemonCreated));
-});
-
-app.put('/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const pokemonUpdate = {...req.body, id: id};
-    pokemons = pokemons.map(pokemon => {
-        return pokemon.id === id ? pokemonUpdate : pokemon
-    });
-    const message = "un pokemon a ete modifier";
-    res.json(success(message, pokemonUpdate));
-});
-
-app.delete('/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id);
-    pokemons = pokemons.filter(pokemon => pokemon.id !== id);
-    const message = "un pokemon a ete supprimer";
-    res.json(success(message, pokemonDeleted));
-});
-
-const port = 3000;
 app.listen(port, () => {
     console.log("notre application ecoute http://localhost:" + port + "/")
 })
--- helper.js
+++
@@ -1,9 +0,0 @@
-exports.success = (message, data) => {
-    return {message, data}
-}
-
-exports.getUniqueId = (pokemons) => {
-    const pokemonIds = pokemons.map(pokemon => pokemon.id);
-    const maxId = pokemonIds.reduce((a, b) => Math.max(a, b));
-    return maxId + 1;
-}
