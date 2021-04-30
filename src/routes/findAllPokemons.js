83c49437b70c8763ccaa9e08397b9038bcd3a553
tarak <bouziri.tarak@gmail.com>
2021-04-30 02:58:01 -0400

    Recherche approximative avec le paramètre name

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,10 +1,17 @@
+const {Op} = require("sequelize");
 const {Pokemon} = require('../db/sequelize')
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
         if (req.query.name) {
             const name = req.query.name;
-            return Pokemon.findAll({where: {name: name}})
+            return Pokemon.findAll({
+                where: {
+                    name: {
+                        [Op.like]: '%' + name + '%'
+                    }
+                }
+            })
                 .then(pokemons => {
                     const message = 'il y a ' + pokemons.length + ' pokémons qui correspondent a votre recherche ' + name;
                     res.json({message, data: pokemons})
