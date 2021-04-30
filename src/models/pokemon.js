5f228f6de326cb5215d6d1e56766136d623db15b
tarak <bouziri.tarak@gmail.com>
2021-04-30 01:36:45 -0400

    Ajout des erreurs de validations

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -7,19 +7,35 @@ module.exports = (sequelize, DataTypes) => {
         },
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: {msg: "le nom du pokemon ne doit pas etre vide"},
+                notNull: {msg: "le nom du pokemon est une proprietee requise"}
+            }
         },
         hp: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isInt: {msg: "utiliser uniquement des nombres entier pour les points de vie"},
+                notNull: {msg: "les points de vie sont une proprietee requise"}
+            }
         },
         cp: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isInt: {msg: "utiliser uniquement des nombres entier pour les points de degats"},
+                notNull: {msg: "les points de degats sont une proprietee requise"}
+            }
         },
         picture: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isUrl: {msg: "utiliser un url valide pour les images"},
+                notNull: {msg: "l'image est une proprietee requise"}
+            }
         },
         types: {
             type: DataTypes.STRING,
