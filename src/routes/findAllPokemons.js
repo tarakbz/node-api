7f081af5a6629015bf0e926eea029d8d427e7077
tarak <bouziri.tarak@gmail.com>
2021-04-30 02:56:59 -0400

    Recherche exacte avec le parametre name

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -2,6 +2,14 @@ const {Pokemon} = require('../db/sequelize')
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
+        if (req.query.name) {
+            const name = req.query.name;
+            return Pokemon.findAll({where: {name: name}})
+                .then(pokemons => {
+                    const message = 'il y a ' + pokemons.length + ' pokémons qui correspondent a votre recherche ' + name;
+                    res.json({message, data: pokemons})
+                })
+        }
         Pokemon.findAll()
             .then(pokemons => {
                 const message = 'La liste des pokémons a bien été récupérée.'
