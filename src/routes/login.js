586a00c4fb1cecd702221e1fe9787fdb9891abd3
tarak <bouziri.tarak@gmail.com>
2021-05-02 04:44:34 -0400

    gestion des erreurs du login

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,12 +4,25 @@ const bcrypt = require('bcrypt')
 module.exports = (app) => {
     app.post('/api/login', (req, res) => {
         User.findOne({where: {username: req.body.username}}).then(user => {
+            if (!user) {
+                const message = `L'utilisateur n'existe pas.`;
+                return res.status(404).json({message})
+            }
             bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+
+                if (!isPasswordValid) {
+                    const message = `Le mot de passe est incorrect`;
+                    return res.status(401).json({message})
+                }
                 if (isPasswordValid) {
                     const message = `L'utilisateur a été connecté avec succès`;
                     return res.json({message, data: user})
                 }
             })
         })
+            .catch(error => {
+                const message = `L'utilisateur n'a pas pu se connecter`;
+                return res.json({message, data: error})
+            })
     })
 }
