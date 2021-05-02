bea453b31ccf1961eb7149829b2f635630a3e66c
tarak <bouziri.tarak@gmail.com>
2021-05-01 23:56:40 -0400

    Encrypter le mot de passe

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,4 +1,5 @@
 const {Sequelize, DataTypes} = require('sequelize')
+const bcrypt = require('bcrypt')
 const PokemonModel = require('../models/pokemon')
 const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
@@ -13,6 +14,7 @@ const User = UserModel(sequelize, DataTypes);
 
 const initDb = () => {
     return sequelize.sync({force: true}).then(_ => {
+        console.log('Init DB')
         pokemons.map(pokemon => {
             Pokemon.create({
                 name: pokemon.name,
@@ -21,15 +23,19 @@ const initDb = () => {
                 picture: pokemon.picture,
                 types: pokemon.types
             }).then(pokemon => console.log(pokemon.toJSON()))
-        })
-        User.create({
-            username : "advil",
-            password : "advil"
-        }).then(user => console.log(user.toJSON()))
+        });
+        bcrypt.hash('advil', 10)
+            .then(hash => {
+                User.create({
+                    username: "advil",
+                    password: hash
+                }).then(user => console.log(user.toJSON()))
+            })
+
         console.log('La base de donnée a bien été initialisée !')
     })
 }
 
 module.exports = {
-    initDb, Pokemon , User
+    initDb, Pokemon, User
 }
